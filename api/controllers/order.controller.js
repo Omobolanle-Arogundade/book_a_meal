e3a2e9eb9cb8f8e55d0e66f2e121911a237d9c57
Bolanle Bincom <bolanle.bincom@gmail.com>
2019-02-16 20:41:14 +0100

    added comments to OrderController

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -3,6 +3,14 @@ import { isObject } from 'util';
 import OrderService from '../services/order.service';
 
 const OrderController = {
+
+  /**
+ * fetchAllOrders methods gets all the Order from
+ * the OrderService's fetchOrders methods and returns
+ * a json response with status and data
+ * @param req
+ * @param res
+ */
   fetchOrders(req, res) {
     const allOrders = OrderService.fetchAllOrders();
     return res.json({
@@ -11,6 +19,13 @@ const OrderController = {
     }).status(200);
   },
 
+  /**
+   * addOrder methods gets the newOrder passed in the request body
+   * and uses the OrderService's addOrder method to add order to the orders array
+   * and returns a json response with status and data
+   * @param req
+   * @param res
+   */
   addOrder(req, res) {
     const newOrder = req.body;
     const addedOrder = OrderService.addNewOrder(newOrder);
@@ -21,6 +36,14 @@ const OrderController = {
     });
   },
 
+  /**
+ * The updateOrder method gets the id passed in the request params
+ * and the new value of the order passes in the post bosy
+ * and then uses the OrderService's updateOrder method to find and
+ * update the order with the provided index and data
+ * @param {*} req
+ * @param {*} res
+ */
   updateOrder(req, res) {
     const { id } = req.params;
     const orderValue = req.body;
