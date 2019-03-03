1e27309cc28541980791e694edaa0383495a95f0
Bolanle Bincom <bolanle.bincom@gmail.com>
2019-03-03 16:40:48 +0100

    Added seeders for roles

---
+++ api/config/permissions.js
@@ -0,0 +1,12 @@
+const permissions = {
+  0: 'global',
+  1: 'readUuser',
+  2: 'writeAdmin',
+  3: 'writeMenu',
+  4: 'readMenu',
+  5: 'writeMeal',
+  6: 'readMeal',
+  7: 'writeOrder',
+  8: 'readOrder',
+};
+export default permissions;
