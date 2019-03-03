58d3615132ecf74421cd9bfcb67ca56756f68ba8
Bolanle Bincom <bolanle.bincom@gmail.com>
2019-03-03 12:06:09 +0100

    Created environment setup

---
+++ api/database/migrations/20190303101659-create-roles.js
@@ -0,0 +1,31 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Roles', {
+    role_id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    role_name: {
+      type: Sequelize.STRING,
+    },
+    role_description: {
+      type: Sequelize.STRING,
+    },
+    role_permissions: {
+      type: Sequelize.ARRAY(Sequelize.INTEGER),
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    deletedAt: {
+      type: Sequelize.DATE,
+    },
+  }),
+  down: queryInterface => queryInterface.dropTable('Roles'),
+};
