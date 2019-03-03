a15cfdf156a2b5ec7625c866d9d637dcbba68d42
Bolanle Bincom <bolanle.bincom@gmail.com>
2019-03-03 21:41:08 +0100

    Completed signUp endpoint

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,40 @@
+import db from '../database/models';
+import hashPassword from '../services/hash-password';
+
+const { Users } = db;
+
+export default class AuthController {
+  /**
+   *
+   * @param {*} req
+   * @param {*} res
+   */
+  static async addUser(req, res) {
+    try {
+    /*eslint-disable */
+      const role_id = req.role_id ? req.role_id : 3;
+      const {
+        user_firstName,
+        user_lastName,
+        user_email,
+        user_phoneNo,
+        user_password,
+      } = req.body;
+   /* eslint-enable */
+      const password = hashPassword(user_password);
+      const user = await Users.create({
+        user_firstName, user_lastName, user_email, user_phoneNo, user_password: password, role_id,
+      });
+      return res.status(201).json({
+        status: 'success',
+        message: 'User succesfully Registered',
+        user,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+  }
+}
--- api/database/migrations/20190303104742-create-users.js
+++ api/database/migrations/20190303104742-create-users.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
-    user_id: {
+    id: {
       allowNull: false,
       autoIncrement: true,
       primaryKey: true,
