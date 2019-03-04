62b2d96a14c2ca72631682571f09b88d434affae
Bolanle Bincom <bolanle.bincom@gmail.com>
2019-03-04 16:18:39 +0100

    added meal model and migratiions

---
+++ api/database/migrations/20190304142559-create-meals.js
@@ -0,0 +1,49 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Meals', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    user_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'id',
+        as: 'user_id',
+      },
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    image_url: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    deleted: {
+      type: Sequelize.INTEGER,
+      defaultValue: 0,
+      allowNull: true,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: queryInterface => queryInterface.dropTable('Meals'),
+};
---
+++ api/database/seeders/20190304143906-meals.js
@@ -0,0 +1,24 @@
+const date = new Date();
+
+module.exports = {
+  up: queryInterface => queryInterface.bulkInsert('Meals', [{
+    name: 'Fried Rice and Chicken',
+    description: 'Grilled chicken with seasoned fried Rice',
+    price: 1100,
+    user_id: 1,
+    image_url: 'https://image1.com',
+    createdAt: date,
+    updatedAt: date,
+  },
+  {
+    name: 'Pounded Yam and Vegetable soup',
+    description: 'A blend of strong pounded yam and assorted vegetable soup',
+    price: 500,
+    user_id: 1,
+    image_url: 'https://image2.com',
+    createdAt: date,
+    updatedAt: date,
+  }], {}),
+
+  down: queryInterface => queryInterface.bulkDelete('Users', null, {}),
+};
