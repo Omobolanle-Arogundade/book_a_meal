0e37089eaf66582c60e7cba48b472b52131ab13f
Bolanle Bincom <bolanle.bincom@gmail.com>
2019-03-09 07:00:09 +0100

    Removed order model

--- api/config/config.js
+++ api/config/config.js
@@ -17,7 +17,11 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    use_env_variable: 'postgres://exqkflcahvpvwo:fad87feb4a442d3387f7d85211beaea37a02f15d19c1a0a4877bab362508575e@ec2-54-75-232-114.eu-west-1.compute.amazonaws.com:5432/ddqgg2buoimjvl',
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
   },
   travis: {
     username: 'tester',
