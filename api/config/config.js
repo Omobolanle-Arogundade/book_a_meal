d390b71cb6b2e7fa0dbb657d7472eee1294d8417
Bolanle Bincom <bolanle.bincom@gmail.com>
2019-03-09 07:10:17 +0100

    ..

--- api/config/config.js
+++ api/config/config.js
@@ -17,7 +17,7 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    use_env_variable: 'postgres://exqkflcahvpvwo:fad87feb4a442d3387f7d85211beaea37a02f15d19c1a0a4877bab362508575e@ec2-54-75-232-114.eu-west-1.compute.amazonaws.com:5432/ddqgg2buoimjvl',
+    DATABASE_URL: 'postgres://exqkflcahvpvwo:fad87feb4a442d3387f7d85211beaea37a02f15d19c1a0a4877bab362508575e@ec2-54-75-232-114.eu-west-1.compute.amazonaws.com:5432/ddqgg2buoimjvl',
   },
   travis: {
     username: 'tester',
